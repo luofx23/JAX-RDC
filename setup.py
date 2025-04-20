89f00d22720856cdc84bbf2961d72bfc717f6153
FaxuanLuo <88180662+luofx23@users.noreply.github.com>
2025-04-20 19:43:55 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="janc",
     version="0.1.0",
-    package_dir={"janc": "src"},
+    package_dir={"": "src"},
     packages=find_packages(where="src"),
     install_requires=[
         'numpy>=1.21',
