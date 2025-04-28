981b96b174ed9d740961c3441a60e5b88eb43ac8
FaxuanLuo <88180662+luofx23@users.noreply.github.com>
2025-04-28 10:52:14 +0800

    Update chemical.py

--- src/janc/thermodynamics/chemical.py
+++ src/janc/thermodynamics/chemical.py
@@ -68,8 +68,8 @@ def construct_matrix_equation(T,X,dt):
     dwk_drhonYn = dwk_drhonYn[0:-1,0:-1,:,:]
     S = jnp.transpose(w_k,(2,3,0,1))
     DSDU = jnp.transpose(dwk_drhonYn,(2,3,0,1))
-    Idim = (thermo.n)-1
-    I = jnp.eye(Idim)
+    Idim = (thermo.n)
+    I = jnp.eye(thermo.n)
     A = I/dt - DSDU
     b = S
     return A, b
