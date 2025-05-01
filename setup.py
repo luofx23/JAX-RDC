105349515c47d8bba9864b8ce932bfdbc9d034d9
FaxuanLuo <88180662+luofx23@users.noreply.github.com>
2025-05-01 14:16:54 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     package_dir={"": "src"},
     install_requires=[
         'cantera',
-        'jaxamr @ git+https://github.com/JA4S/JAX-AMR.git'
+        'pyCGNS'
     ],
     python_requires=">=3.7"
 )
