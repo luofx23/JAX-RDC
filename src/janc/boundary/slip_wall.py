ec73f31b4647a7ab2496920876bd4a2889cf449b
FaxuanLuo <88180662+luofx23@users.noreply.github.com>
2025-05-01 16:38:23 +0800

    Add files via upload

--- src/janc/boundary/slip_wall.py
+++ src/janc/boundary/slip_wall.py
@@ -3,48 +3,56 @@ from ..grid import read_grid
 
 def left(U_bd):
     U_bd_ghost = jnp.concatenate([U_bd[:,2:3,:],U_bd[:,1:2,:],U_bd[:,0:1,:]],axis=1)
-    U_norm = jnp.sqrt((U_bd_ghost[1:2]/U_bd_ghost[0:1])**2 + (U_bd_ghost[2:3]/U_bd_ghost[0:1])**2)
-    Ux_n = U_norm*read_grid.nx_L
-    Uy_n = U_norm*read_grid.ny_L
-    Ux_t = (U_bd_ghost[1:2]/U_bd_ghost[0:1]) - Ux_n
-    Uy_t = (U_bd_ghost[2:3]/U_bd_ghost[0:1]) - Uy_n
-    Ux = Ux_t - Ux_n
-    Uy = Uy_t - Uy_n
-    U_bd_ghost = U_bd_ghost.at[1:3].set(U_bd_ghost[0:1]*jnp.concatenate([Ux,Uy],axis=0))
+    u = U_bd_ghost[1:2]/U_bd_ghost[0:1]
+    v = U_bd_ghost[2:3]/U_bd_ghost[0:1]
+    U_n = u*read_grid.nx_L + v*read_grid.ny_L
+    u_n = U_n*read_grid.nx_L
+    v_n = U_n*read_grid.ny_L
+    u_t = u - u_n
+    v_t = v - v_n
+    u = u_t - u_n
+    v = v_t - v_n
+    U_bd_ghost = U_bd_ghost.at[1:3].set(U_bd_ghost[0:1]*jnp.concatenate([u,v],axis=0))
     return U_bd_ghost
 
 def right(U_bd):
     U_bd_ghost = jnp.concatenate([U_bd[:,-1:,:],U_bd[:,-2:-1,:],U_bd[:,-3:-2,:]],axis=1)
-    U_norm = jnp.sqrt((U_bd_ghost[1:2]/U_bd_ghost[0:1])**2 + (U_bd_ghost[2:3]/U_bd_ghost[0:1])**2)
-    Ux_n = U_norm*read_grid.nx_R
-    Uy_n = U_norm*read_grid.ny_R
-    Ux_t = (U_bd_ghost[1:2]/U_bd_ghost[0:1]) - Ux_n
-    Uy_t = (U_bd_ghost[2:3]/U_bd_ghost[0:1]) - Uy_n
-    Ux = Ux_t - Ux_n
-    Uy = Uy_t - Uy_n
-    U_bd_ghost = U_bd_ghost.at[1:3].set(U_bd_ghost[0:1]*jnp.concatenate([Ux,Uy],axis=0))
+    u = U_bd_ghost[1:2]/U_bd_ghost[0:1]
+    v = U_bd_ghost[2:3]/U_bd_ghost[0:1]
+    U_n = u*read_grid.nx_R + v*read_grid.ny_R
+    u_n = U_n*read_grid.nx_R
+    v_n = U_n*read_grid.ny_R
+    u_t = u - u_n
+    v_t = v - v_n
+    u = u_t - u_n
+    v = v_t - v_n
+    U_bd_ghost = U_bd_ghost.at[1:3].set(U_bd_ghost[0:1]*jnp.concatenate([u,v],axis=0))
     return U_bd_ghost
 
 def bottom(U_bd):
     U_bd_ghost = jnp.concatenate([U_bd[:,:,2:3],U_bd[:,:,1:2],U_bd[:,:,0:1]],axis=2)
-    U_norm = jnp.sqrt((U_bd_ghost[1:2]/U_bd_ghost[0:1])**2 + (U_bd_ghost[2:3]/U_bd_ghost[0:1])**2)
-    Ux_n = U_norm*read_grid.nx_B
-    Uy_n = U_norm*read_grid.ny_B
-    Ux_t = (U_bd_ghost[1:2]/U_bd_ghost[0:1]) - Ux_n
-    Uy_t = (U_bd_ghost[2:3]/U_bd_ghost[0:1]) - Uy_n
-    Ux = Ux_t - Ux_n
-    Uy = Uy_t - Uy_n
-    U_bd_ghost = U_bd_ghost.at[1:3].set(U_bd_ghost[0:1]*jnp.concatenate([Ux,Uy],axis=0))
+    u = U_bd_ghost[1:2]/U_bd_ghost[0:1]
+    v = U_bd_ghost[2:3]/U_bd_ghost[0:1]
+    U_n = u*read_grid.nx_B + v*read_grid.ny_B
+    u_n = U_n*read_grid.nx_B
+    v_n = U_n*read_grid.ny_B
+    u_t = u - u_n
+    v_t = v - v_n
+    u = u_t - u_n
+    v = v_t - v_n
+    U_bd_ghost = U_bd_ghost.at[1:3].set(U_bd_ghost[0:1]*jnp.concatenate([u,v],axis=0))
     return U_bd_ghost
 
 def up(U_bd):
     U_bd_ghost = jnp.concatenate([U_bd[:,:,-1:],U_bd[:,:,-2:-1],U_bd[:,:,-3:-2]],axis=2)
-    U_norm = jnp.sqrt((U_bd_ghost[1:2]/U_bd_ghost[0:1])**2 + (U_bd_ghost[2:3]/U_bd_ghost[0:1])**2)
-    Ux_n = U_norm*read_grid.nx_U
-    Uy_n = U_norm*read_grid.ny_U
-    Ux_t = (U_bd_ghost[1:2]/U_bd_ghost[0:1]) - Ux_n
-    Uy_t = (U_bd_ghost[2:3]/U_bd_ghost[0:1]) - Uy_n
-    Ux = Ux_t - Ux_n
-    Uy = Uy_t - Uy_n
-    U_bd_ghost = U_bd_ghost.at[1:3].set(U_bd_ghost[0:1]*jnp.concatenate([Ux,Uy],axis=0))
+    u = U_bd_ghost[1:2]/U_bd_ghost[0:1]
+    v = U_bd_ghost[2:3]/U_bd_ghost[0:1]
+    U_n = u*read_grid.nx_U + v*read_grid.ny_U
+    u_n = U_n*read_grid.nx_U
+    v_n = U_n*read_grid.ny_U
+    u_t = u - u_n
+    v_t = v - v_n
+    u = u_t - u_n
+    v = v_t - v_n
+    U_bd_ghost = U_bd_ghost.at[1:3].set(U_bd_ghost[0:1]*jnp.concatenate([u,v],axis=0))
     return U_bd_ghost
