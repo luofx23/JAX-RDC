85173330ce16d838172c52b0beeb8332a2f1f01a
FaxuanLuo <88180662+luofx23@users.noreply.github.com>
2025-05-02 10:53:20 +0800

    Update boundary_padding.py

--- src/janc/boundary/boundary_padding.py
+++ src/janc/boundary/boundary_padding.py
@@ -2,27 +2,27 @@ import jax
 import jax.numpy as jnp
 
 def pad(field):
-    field_periodic_x = jnp.concatenate([field[:,-3:-2,:],field[:,-2:-1,:],field,field[:,1:2,:],field[:,2:3,:]],axis=1)
-    field_periodic_pad = jnp.concatenate([field_periodic_x[:,:,-3:-2],field_periodic_x[:,:,-2:-1],field_periodic_x,field_periodic_x[:,:,1:2],field_periodic_x[:,:,2:3]],axis=2)
+    field_periodic_x = jnp.concatenate([field[:,-4:-3,:],field[:,-3:-2,:],field[:,-2:-1,:],field,field[:,1:2,:],field[:,2:3,:],field[:,3:4,:]],axis=1)
+    field_periodic_pad = jnp.concatenate([field_periodic_x[:,:,-4:-3],field_periodic_x[:,:,-3:-2],field_periodic_x[:,:,-2:-1],field_periodic_x,field_periodic_x[:,:,1:2],field_periodic_x[:,:,2:3],field_periodic_x[:,:,3:4]],axis=2)
     return field_periodic_pad
 
 def replace_lb(U_bd, padded_U):
-    U = padded_U.at[:,0:3,2:-2].set(U_bd)
+    U = padded_U.at[:,0:3,3:-3].set(U_bd)
     return U
 
     
 def replace_rb(U_bd, padded_U):
-    U = padded_U.at[:,-3:,2:-2].set(U_bd)
+    U = padded_U.at[:,-3:,3:-3].set(U_bd)
     return U
 
 
 def replace_ub(U_bd, padded_U):
-    U = padded_U.at[:,2:-2,-3:].set(U_bd)
+    U = padded_U.at[:,3:-3,-3:].set(U_bd)
     return U
 
 
 
 def replace_bb(U_bd, padded_U):  
-    U = padded_U.at[:,2:-2,0:3].set(U_bd)
+    U = padded_U.at[:,3:-3,0:3].set(U_bd)
     return U
     
